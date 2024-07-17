8248ff755d0805e2113d89414cd2011ae4dffbb7
sbondoc <42726060+sbondoc@users.noreply.github.com>
2024-07-17 02:11:29 -0700

    Add multi-file extension support

--- bin/extensions.bash
+++ bin/extensions.bash
@@ -2,3 +2,14 @@ export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_CONFIG_HOME="${HOME}/.local/etc"
 export XDG_STATE_HOME="${HOME}/.local/var/lib"
 export XDG_CACHE_HOME="${HOME}/.local/var/cache"
+
+if [ -d "${XDG_CONFIG_HOME}"/bash/src/extensions ]
+then
+    for ext in sh bash
+    do
+        for f in "${XDG_CONFIG_HOME}"/bash/src/extensions/*.${ext}
+        do
+            [ -r "${f}" ] && . "${f}"
+	done
+    done
+fi
