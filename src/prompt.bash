4aca84756731081a954c999052710b8c8a775782
sbondoc <42726060+sbondoc@users.noreply.github.com>
2024-07-19 00:07:04 -0700

    Move main prompt functionality to function

--- src/prompt.bash
+++ src/prompt.bash
@@ -62,58 +62,63 @@ __ps1_replace()
     unset right
 }
 
-len_var=40
-len_dir_max=$((${len_var} / 2))
-dir_name="${PWD#${HOME}}"
-if [ ${#PWD} -ne ${#dir_name} ]
-then
-    dir_name="~${dir_name}"
-fi
-if [ ${#dir_name} -le ${len_dir_max} ]
-then
-    len_dir=${#dir_name}
-else
-    len_dir=${len_dir_max}
-fi
-len_branch_max=$((${len_var} - ${len_dir}))
-unset __git_ps1_branch_name
-__git_ps1 "" "" "%s"
-branch_name=${__git_ps1_branch_name}
-if [ $((${#branch_name} + 1)) -le ${len_branch_max} ]
-then
-    if [ -z ${branch_name} ]
+__ps1()
+{
+    len_var=${1}
+    len_dir_max=$((${len_var} / 2))
+    dir_name="${PWD#${HOME}}"
+    if [ ${#PWD} -ne ${#dir_name} ]
     then
-        len_branch=0
-    else
-        len_branch=$((${#branch_name} + 1))
+        dir_name="~${dir_name}"
     fi
-    len_dir=$((${len_var} - ${len_branch}))
-else
-    len_branch=${len_branch_max}
-fi
-__ps1_trunc tail ${len_dir} "${dir_name}"
-printf "%s" "${__PS1_TRUNC_OUT_ELLIPSIS}"
-printf "%s" "\[\e[01;34m\]"
-printf "%s" "${__PS1_TRUNC_OUT}"
-printf "%s" "\[\e[00m\]"
-if [ ${len_branch} -gt 0 ]
-then
-    printf ":"
-    __ps1_trunc head $((${len_branch} - 1)) ${branch_name}
-    case "${PS1}" in
-    ("\\[\\e[31m\\]"*)
-        printf "%s" "\[\e[31m\]"
-    ;;
-    ("\\[\\e[32m\\]"*)
-        printf "%s" "\[\e[32m\]"
-    ;;
-    (*)
-        printf "%s" "\[\e[00m\]"
-    ;;
-    esac
+    if [ ${#dir_name} -le ${len_dir_max} ]
+    then
+        len_dir=${#dir_name}
+    else
+        len_dir=${len_dir_max}
+    fi
+    len_branch_max=$((${len_var} - ${len_dir}))
+    unset __git_ps1_branch_name
+    __git_ps1 "" "" "%s"
+    branch_name=${__git_ps1_branch_name}
+    if [ $((${#branch_name} + 1)) -le ${len_branch_max} ]
+    then
+        if [ -z ${branch_name} ]
+        then
+            len_branch=0
+        else
+            len_branch=$((${#branch_name} + 1))
+        fi
+        len_dir=$((${len_var} - ${len_branch}))
+    else
+        len_branch=${len_branch_max}
+    fi
+    __ps1_trunc tail ${len_dir} "${dir_name}"
+    printf "%s" "${__PS1_TRUNC_OUT_ELLIPSIS}"
+    printf "%s" "\[\e[01;34m\]"
     printf "%s" "${__PS1_TRUNC_OUT}"
     printf "%s" "\[\e[00m\]"
-    printf "%s" "${__PS1_TRUNC_OUT_ELLIPSIS}"
-fi
-printf "%s" "\$ "
+    if [ ${len_branch} -gt 0 ]
+    then
+        printf ":"
+        __ps1_trunc head $((${len_branch} - 1)) ${branch_name}
+        case "${PS1}" in
+        ("\\[\\e[31m\\]"*)
+            printf "%s" "\[\e[31m\]"
+        ;;
+        ("\\[\\e[32m\\]"*)
+            printf "%s" "\[\e[32m\]"
+        ;;
+        (*)
+            printf "%s" "\[\e[00m\]"
+        ;;
+        esac
+        printf "%s" "${__PS1_TRUNC_OUT}"
+        printf "%s" "\[\e[00m\]"
+        printf "%s" "${__PS1_TRUNC_OUT_ELLIPSIS}"
+    fi
+    printf "%s" "\$ "
+}
+
+__ps1 40
 )'
