0acf299cbb96786937c6c66693f6a3ac97687c39
sbondoc <42726060+sbondoc@users.noreply.github.com>
2024-07-21 11:35:21 -0700

    Update quote correctness

--- src/ps1.bash
+++ src/ps1.bash
@@ -1,14 +1,14 @@
 trunc()
 {
-    str=${3}
+    str="${3}"
     if [ ${#str} -le ${2} ]
     then
         trunc_out_ellipsis=
-        trunc_out=${str}
+        trunc_out="${str}"
         unset str
         return
     fi
-    trunc_out_ellipsis=...
+    trunc_out_ellipsis='...'
     q=
     while [ ${#q} -lt $((${2} - ${#trunc_out_ellipsis})) ]
     do
@@ -16,10 +16,10 @@ trunc()
     done
     case ${1} in
     (head)
-        trunc_out=${str%${str#${q}}}
+        trunc_out="${str%${str#${q}}}"
     ;;
     (tail)
-        trunc_out=${str#${str%${q}}}
+        trunc_out="${str#${str%${q}}}"
     ;;
     esac
     unset q
@@ -28,22 +28,22 @@ trunc()
 
 print_color()
 {
-    printf "\["
+    printf '\['
     case ${1} in
     (red)
-        printf "\e[31m"
+        printf '\e[31m'
     ;;
     (green)
-        printf "\e[32m"
+        printf '\e[32m'
     ;;
     (bblue)
-        printf "\e[1;34m"
+        printf '\e[1;34m'
     ;;
     (none)
-        printf "\e[00m"
+        printf '\e[00m'
     ;;
     esac
-    printf "\]"
+    printf '\]'
 }
 
 main()
@@ -63,7 +63,7 @@ main()
     fi
     len_branch_max=$((${len_var} - ${len_dir}))
     unset __git_ps1_branch_name
-    __git_ps1 "" "" "%s"
+    __git_ps1 '' '' '%s'
     name_branch=${__git_ps1_branch_name}
     if [ $((${#name_branch} + 1)) -le ${len_branch_max} ]
     then
@@ -78,27 +78,27 @@ main()
         len_branch=${len_branch_max}
     fi
     trunc tail ${len_dir} "${name_dir}"
-    printf "%s" "${trunc_out_ellipsis}"
+    printf '%s' "${trunc_out_ellipsis}"
     print_color bblue
-    printf "%s" "${trunc_out}"
+    printf '%s' "${trunc_out}"
     print_color none
     if [ ${len_branch} -gt 0 ]
     then
-        printf ":"
+        printf ':'
         trunc head $((${len_branch} - 1)) ${name_branch}
         case "${PS1}" in
-        ("\\[\\e[31m\\]"*)
+        ('\\[\\e[31m\\]'*)
             print_color red
         ;;
-        ("\\[\\e[32m\\]"*)
+        ('\\[\\e[32m\\]'*)
             print_color green
         ;;
         esac
-        printf "%s" "${trunc_out}"
+        printf '%s' "${trunc_out}"
         print_color none
-        printf "%s" "${trunc_out_ellipsis}"
+        printf '%s' "${trunc_out_ellipsis}"
     fi
-    printf "%s" "\$ "
+    printf '$ '
 }
 
 main 40
